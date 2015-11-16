762d052fd34de96859e2734945d001fc25efbe34
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-16 18:55:19 -0500

    Refactor......................

--- routes/popular.js
+++ routes/popular.js
@@ -3,30 +3,15 @@
  */
 var express = require('express');
 var router = express.Router();
-var ig=require('instagram-node').instagram();
+var instagram = require("../lib/instagram");
 
 
-ig.use({
-    client_id:'2d880c21672b4094aa86ef98c7a03064',
-    client_secret:'8e905af2b92144aeae757ada649ab7ab'
-});
-var polular_cache = {};
-function get_most_popular(cb) {
-    var timeout = 5;
-    if(popular_cache.media&&(new Date().getTime()<popular_cache.timeout)){
-        return cb(null, polular_cache.media,popular_cache.limit);
-    }
-    ig.media_popular(function(err,media,limit){
-        polular_cache={
-            "media":media,
-            "limit":limit,
-            "timeout":(+(new Date())) +(timeout * 1000)
-        }
-        return cb(err,media,limit);
-    });
-}
+var ig =new instagram('2d880c21672b4094aa86ef98c7a03064','8e905af2b92144aeae757ada649ab7ab');
+//var polular_cache = {};
+
+
 router.get('/', function(req, res, next) {
-    get_most_popular()(function(err,media,limit){
+    ig.get_most_popular_with_cache(function(err,media,limit){
         if(err){
             throw err;
         }
