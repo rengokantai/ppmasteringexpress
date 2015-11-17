81233160ca9eddf8bf5129e02f307cdc8d21e92e
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-16 19:52:18 -0500

    mocha test add callback and eq, eql

--- test/instagram.js
+++ test/instagram.js
@@ -6,6 +6,10 @@ var Instagram = require('../lib/instagram');
 
 var ig = new Instagram("id","pass");
 var ig2 =new Instagram('2d880c21672b4094aa86ef98c7a03064','8e905af2b92144aeae757ada649ab7ab');
+
+ig.get_most_popular = function(cb){
+    return cb(null,{"data":"incoming"},3);
+}
 describe("credentials",function(){
     it("should be same id", function(){
         expect(ig.getClientId()).to.be("id");
@@ -13,10 +17,12 @@ describe("credentials",function(){
     it("should be same secret", function(){
         expect(ig.getClientSecret()).to.be("pass");
     });
-    it("get media",function(){
-            ig2.get_most_popular(function(err,media,limit){
+    it("get media",function(done){
+            ig.get_most_popular(function(err,media,limit){
                 expect(err).to.be(null);
                 expect(media).to.be.an('object');
+                expect(media).to.eql({"data":"incoming"});
+                expect(limit).to.equal(3);
                 done();
             });
         });
